6fc638246d439bf7bc693be8e38c00c778cf676c
rookmate <14072042+rookmate@users.noreply.github.com>
2023-10-09 22:14:16 +0100

    Improve pages based on access token retrieval

--- pages/gallery/[id].jsx
+++ pages/gallery/[id].jsx
@@ -1,35 +1,49 @@
 import Image from 'next/image';
 import styles from './[id].module.css';
 const Dropbox = require('dropbox').Dropbox;
+import axios from 'axios'; 
 
-const ACCESS_TOKEN = process.env.NEXT_PUBLIC_DROPBOX_API_KEY;
-const dbx = new Dropbox({ accessToken: ACCESS_TOKEN });
+async function retrieveAccessToken() {
+  // Instructions to get the refresh token: https://stackoverflow.com/a/74456272
+  const postData = new URLSearchParams({
+    refresh_token: process.env.NEXT_PUBLIC_DROPBOX_REFRESH_TOKEN,
+    grant_type: 'refresh_token',
+  });
 
-function HouseGallery({ photos, folderName }) {
-  return (
-    <div className={styles.imageGrid}>
-      {photos.map((photo, index) => (
-        <a href={`/gallery/before-and-after/${index}`} // Specify the dynamic route
-          as={`/gallery/${folderName}/before-and-after/${index}`} // Define the actual URL 
-          key={index} className={styles.photo}
-          >
-          <Image
-            src={photo.url}
-            alt={photo.name}
-            width={400}
-            height={300}
-            className={styles.image}
-          />
-          <p className={styles.textCentered}>{photo.name}</p>
-        </a>
-      ))}
-    </div>
-  );
+  const base64authorization = Buffer.from(`${process.env.NEXT_PUBLIC_DROPBOX_APP_KEY}:${process.env.NEXT_PUBLIC_DROPBOX_APP_SECRET}`).toString('base64');
+  const headers = {
+    Authorization: `Basic ${base64authorization}`,
+    'Content-Type': 'application/x-www-form-urlencoded',
+  };
+
+  try {
+    // Make the POST request to retrieve the access token
+    const response = await axios.post('https://api.dropbox.com/oauth2/token', postData.toString(), { headers });
+    const accessToken = response.data.access_token;
+    return accessToken;
+  } catch (error) {
+    // Handle errors
+    console.error('Error retrieving access token:', error.message);
+    return null;
+  }
 }
 
-export async function getServerSideProps({ params }) {
-  const { id } = params;
+async function fetchData() {
+  // Retrieve the access token
+  const accessToken = await retrieveAccessToken();
+
+  if (!accessToken) {
+    // Handle the case where access token retrieval fails
+    return {
+      photos: [],
+      folderName: "",
+    };
+  }
+
+  // Configure Dropbox with the retrieved access token
+  const dbx = new Dropbox({ accessToken: accessToken });
   const photos = [];
+  const folderName = "";
 
   try {
     // Fetch root folder list
@@ -43,7 +57,7 @@ export async function getServerSideProps({ params }) {
           if (searchResponse.result.matches && searchResponse.result.matches.length > 0) {
             for (const match of searchResponse.result.matches) {
               const metadata = match.metadata;
-              if (metadata.name.split('.').slice(0, -1).join('.') === folderName) {
+              if (metadata.name.split('.').slice(0, 1).join('.') === folderName) {
                 let photoURL = "";
                 try {
                   const response = await dbx.sharingCreateSharedLinkWithSettings({ path: metadata.path_display });
@@ -69,26 +83,55 @@ export async function getServerSideProps({ params }) {
         }
       }
       console.log(photos);
-      // Now, photos contains objects with name and URL properties for matching files.
     } else {
       console.log('No folders found in the root folder.');
     }
 
     return {
-      props: {
-        photos,
-        folderName: id,
-      },
+      photos,
+      folderName,
     };
   } catch (error) {
     console.error('Error retrieving photos:', error);
     return {
-      props: {
-        photos: photos,
-        folderName: "",
-      },
+      photos: [],
+      folderName: "",
     };
   }
+}
+
+function HouseGallery({ photos, folderName }) {
+  return (
+    <div className={styles.imageGrid}>
+      {photos.map((photo, index) => (
+        <a href={`/gallery/before-and-after/${index}`} // Specify the dynamic route
+          as={`/gallery/${folderName}/before-and-after/${index}`} // Define the actual URL 
+          key={index} className={styles.photo}
+          >
+          <Image
+            src={photo.url}
+            alt={photo.name}
+            width={400}
+            height={300}
+            className={styles.image}
+          />
+          <p className={styles.textCentered}>{photo.name}</p>
+        </a>
+      ))}
+    </div>
+  );
+}
+
+export async function getServerSideProps({ params }) {
+  // Fetch data using the fetchData function
+  const { photos, folderName } = await fetchData();
+
+  return {
+    props: {
+      photos,
+      folderName,
+    },
+  };
 }
 
 export default HouseGallery;
--- pages/gallery/before-and-after/[before-and-after].jsx
+++ pages/gallery/before-and-after/[before-and-after].jsx
@@ -1,5 +1,105 @@
 import { useRouter } from 'next/router';
 import styles from './[before-and-after].module.css'; // Import your CSS file
+import { Dropbox } from 'dropbox';
+import axios from 'axios'; 
+
+// Comparison function to sort by name
+function compareByName(a, b) {
+  const nameA = a.name.toUpperCase();
+  const nameB = b.name.toUpperCase();
+
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  return 0;
+}
+
+// Function to retrieve the access token
+async function retrieveAccessToken() {
+  // Instructions to get the refresh token: https://stackoverflow.com/a/74456272
+  const postData = new URLSearchParams({
+    refresh_token: process.env.NEXT_PUBLIC_DROPBOX_REFRESH_TOKEN,
+    grant_type: 'refresh_token',
+  });
+
+  const base64authorization = Buffer.from(`${process.env.NEXT_PUBLIC_DROPBOX_APP_KEY}:${process.env.NEXT_PUBLIC_DROPBOX_APP_SECRET}`).toString('base64');
+  const headers = {
+    Authorization: `Basic ${base64authorization}`,
+    'Content-Type': 'application/x-www-form-urlencoded',
+  };
+
+  try {
+    const response = await axios.post('https://api.dropbox.com/oauth2/token', postData, { headers });
+    const accessToken = response.data.access_token;
+    return accessToken;
+  } catch (error) {
+    // Handle errors
+    console.error('Error:', error.message);
+    return null;
+  }
+}
+
+// Function to find files with the same name as their parent folder
+async function getServerSideProps({ params }) {
+  const folderSelected = params.folderName; // Get the folder name from URL params
+  const beforeImages = [];
+  const afterImages = [];
+
+  // Retrieve the access token
+  const ACCESS_TOKEN = await retrieveAccessToken();
+
+  if (!ACCESS_TOKEN) {
+    // Handle the case where access token retrieval fails
+    return {
+      props: {
+        beforeImages: [],
+        afterImages: [],
+      },
+    };
+  }
+
+  // Initialize Dropbox with the provided access token
+  const dbx = new Dropbox({ accessToken: ACCESS_TOKEN });
+
+  try {
+    const response = await dbx.filesListFolder({ path: '/' + folderSelected });
+
+    response.result.entries.forEach(function (entry) {
+      if (entry['.tag'] === 'file') {
+        if (entry.name.includes('after')) {
+          afterImages.push(entry);
+        }
+
+        if (entry.name.includes('before')) {
+          beforeImages.push(entry);
+        }
+      }
+    });
+
+    // Sort the images by name
+    beforeImages.sort(compareByName);
+    afterImages.sort(compareByName);
+
+    return {
+      props: {
+        beforeImages,
+        afterImages,
+      },
+    };
+  } catch (error) {
+    console.error('Dropbox API error:', error);
+
+    return {
+      props: {
+        beforeImages: [],
+        afterImages: [],
+      },
+    };
+  }
+}
 
 function BeforeAndAfter({ beforeImages, afterImages }) {
   const router = useRouter();
@@ -21,10 +121,10 @@ function BeforeAndAfter({ beforeImages, afterImages }) {
       {/* Render a pair of images */}
       <div className={styles.imagePair}>
         <div className={styles.column}>
-          <img src={beforeImage.url} alt={beforeImage.name} />
+          <Image src={beforeImage.url} alt={beforeImage.name} />
         </div>
         <div className={styles.column}>
-          <img src={afterImage.url} alt={afterImage.name} />
+          <Image src={afterImage.url} alt={afterImage.name} />
         </div>
       </div>
       {/* Add your images and other content here */}
@@ -32,4 +132,5 @@ function BeforeAndAfter({ beforeImages, afterImages }) {
   );
 }
 
+export { getServerSideProps };
 export default BeforeAndAfter;
